7cce233c926123404c62bde20b242a816f6531c5
Martin Hořeňovský <martin.horenovsky@gmail.com>
2015-12-07 12:57:09 +0100

    Completed Day4 with Python.

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -3,15 +3,24 @@ from itertools import count
 
 
 base_string = "bgvyzdsv"
-target_prefix = "00000"
+target_prefix_easy = "00000"
+target_prefix_hard = "000000"
 
-for i in count(0):
-    hasher = hashlib.md5()
-    hasher.update(bytes(base_string + str(i), 'utf-8'))
-    digest = hasher.hexdigest()
-    if (i % 1000 == 0):
-        print(i)
-    if (digest[0:5] == target_prefix):
-        print(i)
-        break
+def n_prefixed(n):
+    target_prefix = n * '0'
+    for i in count(0):
+        hasher = hashlib.md5()
+        hasher.update(bytes(base_string + str(i), 'utf-8'))
+        digest = hasher.hexdigest()
+        if (digest[0:n] == target_prefix):
+            return i
 
+def easy():
+    return n_prefixed(5)
+
+
+def hard():
+    return n_prefixed(6)
+
+print(easy())
+print(hard())
