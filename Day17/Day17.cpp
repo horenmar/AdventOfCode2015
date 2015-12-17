bf677e6ac6ab462bf57a69ee2fc733e7b0f8034f
Martin Hořeňovský <martin.horenovsky@gmail.com>
2015-12-17 11:46:54 +0100

    Added solution for Day 17 second part.

--- Day17/Day17.cpp
+++ Day17/Day17.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
+#include <numeric>
 
 int main() {
     std::vector<int> ways(151);
@@ -19,7 +21,31 @@ int main() {
             ways[w] += ways[w - c];
         }
     }
+    std::cout << "Easy: " << ways[150] << std::endl;
 
-    std::cout << ways[150] << std::endl;
+    // Lets make an ad-hoc combination selector. 
+    // Oh god.
+    int least = std::numeric_limits<int>::max();
+    int how_many = 0;
+    for (int k = 1; k < containers.size(); ++k) {
+        std::vector<bool> selection(containers.size());
+        std::fill(begin(selection), begin(selection) + k, true);
+        if (k > least) {
+            break;
+        }
+        do {
+            std::vector<int> selected;
+            for (int i = 0; i < containers.size(); ++i) {
+                if (selection[i]) {
+                    selected.push_back(containers[i]);
+                }
+            }
+            if (std::accumulate(begin(selected), end(selected), 0) == 150) {
+                least = k;
+                how_many++;
+            }
+        } while (std::prev_permutation(begin(selection), end(selection)));
+    }
 
+    std::cout << "Hard: " << how_many << std::endl;
 }
